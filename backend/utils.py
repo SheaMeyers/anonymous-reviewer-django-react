87fbdc2edd0e2034ebb5a81ba486e649c5957eb2
Shea Meyers <shea.meyers@gmail.com>
2020-11-17 21:37:13 +0100

    Remove unneeded lines of code

--- backend/utils.py
+++ backend/utils.py
@@ -11,30 +11,20 @@ def calculate_star_ratings(additional_filters=None):
 
         reviews = company.reviews.all()
         total_star_review_value = 0
-        number_5_star_reviews = 0
-        number_4_star_reviews = 0
-        number_3_star_reviews = 0
-        number_2_star_reviews = 0
-        number_1_star_reviews = 0
 
         for review in reviews:
             if review.rating == 5:
-                number_5_star_reviews += 1
+                company.rating_stats.number_five_star_ratings += 1
             if review.rating == 4:
-                number_4_star_reviews += 1
+                company.rating_stats.number_four_star_ratings += 1
             if review.rating == 3:
-                number_3_star_reviews += 1
+                company.rating_stats.number_three_star_ratings += 1
             if review.rating == 2:
-                number_2_star_reviews += 1
+                company.rating_stats.number_two_star_ratings += 1
             if review.rating == 1:
-                number_1_star_reviews += 1
+                company.rating_stats.number_one_star_ratings += 1
 
             total_star_review_value += review.rating
 
-        company.rating_stats.number_five_star_ratings = number_5_star_reviews
-        company.rating_stats.number_four_star_ratings = number_4_star_reviews
-        company.rating_stats.number_three_star_ratings = number_3_star_reviews
-        company.rating_stats.number_two_star_ratings = number_2_star_reviews
-        company.rating_stats.number_one_star_ratings = number_1_star_reviews
         company.rating_stats.average_rating = total_star_review_value/reviews.count()
         company.rating_stats.save()
