aee5241ce864cca07f01bf93c97c04636d9cd5da
Shea Meyers <shea.meyers@gmail.com>
2020-12-06 11:43:12 +0100

    Fix google recaptcha key

--- src/keys.tsx
+++ src/keys.tsx
@@ -5,6 +5,6 @@ const domainUrl = 'https://www.anonymousreviewer.com'
 // const googleRecaptchaSiteKey = '6Le6pKcZAAAAAACMEoQ4yHOK_kNyYNiONeFkCqIN';
 
 // prod
-const googleRecaptchaSiteKey = '6Le6pKcZAAAAAACMEoQ4yHOK_kNyYNiONeFkCqIN';
+const googleRecaptchaSiteKey = '6LdIo6cZAAAAAAgiLRMwiKNYUmSv4oFR7oMXvlkb';
 
 export { domainUrl, googleRecaptchaSiteKey };
