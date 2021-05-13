483d7fcd7aeb764aecf8cf1dc39d94d227a85054
Shea Meyers <shea.meyers@gmail.com>
2021-05-13 13:45:56 +0200

    Add proper reactjs keys

--- src/keys.tsx
+++ src/keys.tsx
@@ -1,7 +1,7 @@
-const domainUrl = 'http://localhost:8000'
+const domainUrl = 'https://anonymous-reviewer-app.herokuapp.com/'
 
 // TODO Add recaptcha key here
 //   https://developers.google.com/recaptcha/docs/invisible
-const googleRecaptchaSiteKey = '';
+const googleRecaptchaSiteKey = process.env.GOOGLE_RECAPTCHA_SITE_KEY;
 
 export { domainUrl, googleRecaptchaSiteKey };
