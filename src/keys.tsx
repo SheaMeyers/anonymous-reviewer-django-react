dc2a6209d9528275a10f73a0df80d49cb243c257
Shea Meyers <shea.meyers@gmail.com>
2021-05-13 13:54:43 +0200

    Fix typescript error

--- src/keys.tsx
+++ src/keys.tsx
@@ -2,6 +2,6 @@ const domainUrl = 'https://anonymous-reviewer-app.herokuapp.com/'
 
 // TODO Add recaptcha key here
 //   https://developers.google.com/recaptcha/docs/invisible
-const googleRecaptchaSiteKey = process.env.GOOGLE_RECAPTCHA_SITE_KEY;
+const googleRecaptchaSiteKey = process.env.GOOGLE_RECAPTCHA_SITE_KEY || '';
 
 export { domainUrl, googleRecaptchaSiteKey };
