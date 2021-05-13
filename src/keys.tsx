5c5c0cf0969f11b5abd5976effa8980571906f74
Shea Meyers <shea.meyers@gmail.com>
2021-05-13 14:33:05 +0200

    Update keys and remove logging

--- src/keys.tsx
+++ src/keys.tsx
@@ -1,12 +1,7 @@
-console.log('------------------------');
-console.log('process.env');
-console.log(process.env);
-console.log('------------------------');
-
-const domainUrl = process.env.DOMAIN_URL || 'http://localhost:8000';
+const domainUrl = process.env.REACT_APP_DOMAIN_URL || 'http://localhost:8000';
 
 // TODO Add recaptcha key here
 //   https://developers.google.com/recaptcha/docs/invisible
-const googleRecaptchaSiteKey = process.env.GOOGLE_RECAPTCHA_SITE_KEY || '';
+const googleRecaptchaSiteKey = process.env.REACT_APP_GOOGLE_RECAPTCHA_SITE_KEY || '';
 
 export { domainUrl, googleRecaptchaSiteKey };
