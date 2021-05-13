9ebafb97dce84498b928d6d6e034901f19a1b0af
Shea Meyers <shea.meyers@gmail.com>
2021-05-13 14:49:43 +0200

    Update default value

--- src/keys.tsx
+++ src/keys.tsx
@@ -2,6 +2,6 @@ const domainUrl = process.env.REACT_APP_DOMAIN_URL || 'http://localhost:8000';
 
 // TODO Add recaptcha key here
 //   https://developers.google.com/recaptcha/docs/invisible
-const googleRecaptchaSiteKey = process.env.REACT_APP_GOOGLE_RECAPTCHA_SITE_KEY || 'Your-Recaptcha-Key';
+const googleRecaptchaSiteKey = process.env.REACT_APP_GOOGLE_RECAPTCHA_SITE_KEY || 'Your-Site-Key';
 
 export { domainUrl, googleRecaptchaSiteKey };
